421ba7d18e9c739c94a670ace3d4e5623f84ee7c
Maruf Shafique <marufshafique98@gmail.com>
2018-03-01 02:17:15 +0600

    inital commit

---
+++ js/javascript.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+    var navOffset = 400;
+
+    $(window).scroll(function() {
+        var scrollPos = $(window).scrollTop();
+
+        if( scrollPos >= navOffset) {
+            $("#main-nav").show('slow');
+        } else {
+            $("#main-nav").hide('slow');
+        }
+    })
+})
+
+// Smooth Scroll 
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
